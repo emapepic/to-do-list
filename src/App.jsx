1623cd544a319eb8d096d1278f3391191b06933d
Ema Pepic <emapepic@gmail.com>
2025-10-19 21:26:23 +0200

    added component InputFields

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
+import {useState} from 'react';
 import Header from './components/Header.jsx';
+import InputFields from './components/InputFields.jsx';
 
 function App() {
+  const [showInputFields, setShowInputFields] = useState(false);
+  function addTask() {
+    setShowInputFields(true);
+  }
 
   return (
     <>
       <Header/>
+      {showInputFields && <InputFields/>}
+      <button onClick={addTask}>Add</button>
     </>
   )
 }
---
+++ src/components/InputFields.jsx
@@ -0,0 +1,10 @@
+export default function InputFields () {
+    return (
+      <div>
+        <input type="text" />
+            <input type="checkbox" />
+            <button>Save</button>
+            <button>Delete</button>
+      </div>
+    );
+}
