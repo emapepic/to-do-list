c6723767a1101cf5afe5c620ca73c40cc42c2cc3
Ema Pepic <emapepic@gmail.com>
2025-11-08 23:51:03 +0100

    added date input

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -4,6 +4,7 @@ export default function Task ({taskDesc, deleteTask, completedTask, checked}) {
             <span className={checked ? "completed-list-item" : ""}>{taskDesc}</span>
             <div className="task-btns">
                 <input type="checkbox" onChange={completedTask} checked={checked} />
+                <input type="date" />
                 <button className="delete-btn" onClick={deleteTask}>Delete</button>
             </div>
         </li>
