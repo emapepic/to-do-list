3d60ae1b7cb079ddc85d4a814ce064595880acbd
Ema <emapepic@gmail.com>
2025-12-24 10:43:45 +0100

    revert style for finished tasks

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,7 +1,7 @@
 export default function Task ({taskDesc, deleteTask, completedTask, checked}) {
     return (
         <li className="list-items">
-            <span>{taskDesc}</span>
+            <span className={checked ? "completed-list-item" : ""}>{taskDesc}</span>
             <div className="task-btns">
                 <input type="checkbox" onChange={completedTask} checked={checked} />
                 <button className="delete-btn" onClick={deleteTask}>Delete</button>
