df17e4e0058c61f09790b59cc09f2ef7c19a275f
Ema Pepic <emapepic@gmail.com>
2026-01-25 19:17:02 +0100

    added more info about tasks in one item in list

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -21,19 +21,21 @@ export default function Task ({task, deleteTask, completedTask, updateTask}) {
         <CategoryContext.Provider value={{activeCategory}}>
             <li className="list-items">
                 <input className='checked-task' type="checkbox" onChange={completedTask} checked={task.completed} />
-                <div className="task-desc">
-                    {editOn && 
-                        <input 
-                            type="text" 
-                            value={newTaskDesc} 
-                            onChange={(e) => setNewTaskDesc(e.target.value)} />
-                    }
-                    {!editOn && <span className={task.completed ? "completed-list-item" : ""}>{task.text}</span>}
-                </div>
-                <div className="task-more-info">
-                    {task.priority && <span className={`priority-${task.priority}`}>{task.priority}</span>}
-                    {task.category && <span className="category">{task.category}</span>}
-                    {task.dueDate && <span>{task.dueDate}</span>}
+                <div className='task-info'>
+                    <div className="task-desc">
+                        {editOn && 
+                            <input 
+                                type="text" 
+                                value={newTaskDesc} 
+                                onChange={(e) => setNewTaskDesc(e.target.value)} />
+                        }
+                        {!editOn && <span className={task.completed ? "completed-list-item" : ""}>{task.text}</span>}
+                    </div>
+                    <div className="task-more-info">
+                        {task.priority && <span className={`priority priority-${task.priority}`}>{task.priority}</span>}
+                        {task.category && <span className="category">{task.category}</span>}
+                        {task.dueDate && <span className='due-date'>{task.dueDate}</span>}
+                    </div>
                 </div>
                 <div className="task-btns">
                     {editOn && <button className="save-btn" onClick={saveTask}>Save</button>}
